51488806373f2b30cd4d0d96be01c119b21865cf
TheMkrage <metoofarmer@yahoo.com>
2015-09-07 08:48:38 -0700

    Initial Commit

---
+++ MasterTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasterTimer
+//
+//  Created by Matthew Krager on 9/7/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
